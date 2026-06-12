c8e91ca7235981410bd6345134f0f92efd3c8cf2
Pawel Darulewski <pawel.darulewski@gmail.com>
2026-06-12 17:03:07 +0200

    feat(rust): fenix toolchain

--- rust/flake.nix
+++ rust/flake.nix
@@ -4,24 +4,28 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     nixpkgs,
     flake-utils,
+    fenix,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
+        toolchain = fenix.packages.${system}.default.toolchain;
       in {
         formatter = pkgs.nixfmt-tree;
         devShells.default = pkgs.mkShell {
           packages = [
-            pkgs.rustc
-            pkgs.cargo
-            pkgs.clippy
-            pkgs.rustfmt
+            toolchain
+            pkgs.bacon
             pkgs.rust-analyzer
           ];
         };
