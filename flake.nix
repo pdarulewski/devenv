42eb4d5782645152ce7dccf270ed672fa0ec54a9
Pawel Darulewski <pawel.darulewski@gmail.com>
2026-07-19 11:55:40 +0200

    feat(flake): darwin x86_64 ssupport

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "devenv";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-26.05-darwin";
+    nixpkgs-darwin-x86_64.url = "github:NixOS/nixpkgs/nixpkgs-26.05-darwin";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    nixpkgs-darwin-x86_64,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs =
+          import
+          (
+            if system == "x86_64-darwin"
+            then nixpkgs-darwin-x86_64
+            else nixpkgs
+          )
+          {inherit system;};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.alejandra
+            pkgs.nil
+          ];
+        };
+      }
+    );
+}
